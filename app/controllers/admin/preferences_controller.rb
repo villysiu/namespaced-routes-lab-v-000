f62cc6064b9809332bdade2d4b82c37314fc5c0e
Villy Siu <villysiu@gmail.com>
2017-11-09 01:33:42 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -2,7 +2,7 @@ class Admin::PreferencesController < ApplicationController
   def index
     @preference = Preference.create(artist_sort_order: "ASC",
     song_sort_order: "DESC",
-    allow_create_artists: false,
+    allow_create_artists: true,
     allow_create_songs: true)
 
   end
